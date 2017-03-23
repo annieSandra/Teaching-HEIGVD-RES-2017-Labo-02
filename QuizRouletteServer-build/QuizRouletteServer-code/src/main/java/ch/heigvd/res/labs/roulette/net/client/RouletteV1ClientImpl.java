5f51558c9a84e2346219ad327d28578d53be5c6a
sandra <annie.dongmongoumnai@heig-vd.ch>
2017-03-23 10:25:48 +0100

    implémentation du RouletteV1ClientImpl.java

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
@@ -24,45 +24,128 @@ import java.util.logging.Logger;
 public class RouletteV1ClientImpl implements IRouletteV1Client {
 
   private static final Logger LOG = Logger.getLogger(RouletteV1ClientImpl.class.getName());
+  private static Socket socket = null;
+  private boolean isConnect = false;
+  private static BufferedReader reader;
+  private static PrintWriter writer;
+  
 
   @Override
   public void connect(String server, int port) throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    System.out.println("try to connect on " + server + " : " + port);
+    socket = new Socket(server,port);
+    
+    reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+    
+    String line = null;
+    if((line = reader.readLine()) != null){
+       System.out.println("random sortie 1 serveur" + line);
+       isConnect = true;
+    }
+      
+    
+    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public void disconnect() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    writer.printf(RouletteV1Protocol.CMD_BYE + "\n");
+    writer.flush();
+    
+    writer.close();
+      reader.close();
+      socket.close();
+      isConnect = false;
+    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public boolean isConnected() {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+     return isConnect;
+    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public void loadStudent(String fullname) throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+     writer.printf(RouletteV1Protocol.CMD_LOAD + "\n");
+     writer.flush();
+
+     LOG.log(Level.INFO, "Server : {0}", reader.readLine());
+     writer.printf(fullname + "\n");
+     writer.flush();
+     writer.printf(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER + "\n");
+     writer.flush();
+     LOG.log(Level.INFO, "Server : {0}", reader.readLine());
+     
+    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public void loadStudents(List<Student> students) throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+     writer.printf(RouletteV1Protocol.CMD_LOAD + "\n");
+     LOG.log(Level.INFO, "Server : {0}", reader.readLine());
+     for(Student student:students){
+        writer.printf(student.getFullname() + "\n");
+     }
+     writer.printf(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER + "\n");
+     LOG.log(Level.INFO, "Server : {0}", reader.readLine());
+     writer.flush();
+
+    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public Student pickRandomStudent() throws EmptyStoreException, IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      String line = null;
+     writer.printf(RouletteV1Protocol.CMD_RANDOM + "\n");
+     writer.flush();
+     line = reader.readLine();
+     System.out.println("random sortie 2 serveur" + line);
+     String[] result = line.split(":");
+     if(result.length < 3)
+        throw new EmptyStoreException();
+     return new Student(result[1].substring(1, result[1].length()- 2));
+
+    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public int getNumberOfStudents() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+     String line;
+     writer.printf(RouletteV1Protocol.CMD_INFO + "\n");
+     writer.flush();
+     line = reader.readLine();
+     System.out.println("nombre d'etudiant sortie serveur" + line);
+     String[] result = line.split(":");
+     if(result.length < 3)
+        return 0;
+     System.out.println("nombre d'etudiant resultat split " + result[2]);
+     String numberOfStudentStr = result[2].split("}")[0];
+     System.out.println("nombre d'etudiant " + numberOfStudentStr);
+     if(numberOfStudentStr.equalsIgnoreCase("0")){
+        return 0;
+     }
+     return Integer.parseInt(numberOfStudentStr);
+
+   // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public String getProtocolVersion() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+     
+     String line;
+     writer.printf(RouletteV1Protocol.CMD_INFO + "\n");
+     writer.flush();
+     line = reader.readLine();
+     System.out.println("version sortie serveur" + line);
+     String[] result = line.split(":");
+     System.out.println("version 1er split sortie serveur" + result[1]);
+     String result1 = result[1].split(",")[0];
+     System.out.println("version 2eme split sortie serveur" + result1);
+     return result1.substring(1,result1.length()-1);
+    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
 
